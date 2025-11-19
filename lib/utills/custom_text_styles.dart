90ca0f8089769af1971b7d42087e7dd7ebd2b5a4
waleed <waleed.flutter.tafsol@gmail.com>
2025-11-19 17:14:45 +0500

    custom text class

---
+++ lib/utills/custom_text_styles.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'color_constant.dart';
+
+class CustomTextStyles {
+  static final TextStyle black12w400 = TextStyle(
+  color: Colors.black,
+  fontSize: 12.sp,
+  fontWeight: FontWeight.w400,
+  );
+}
